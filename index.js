f0a922acff1916e916e093a8ca973a9928ceedec
vincentbaylon <vincentbaylon@gmail.com>
2021-06-30 21:08:37 -0700

    commit tree 3b362065535dc13387a0c0f24d232cb895fd11f4

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+// function filter (collection, cb) {
+//     for (const user of collection) {
+//       if (cb(user)) {
+//         console.log(user.firstName);
+//       }
+//     }
+//  }
+  
+function findMatching(driverArr, nameString) {
+    const filterArr = driverArr.filter(driverName => driverName.toLowerCase() === nameString.toLowerCase())
+    return filterArr
+}
+
+function fuzzyMatch(driverArr, argString) {
+    const filterArr = driverArr.filter(driverName => driverName.startsWith(argString))
+    return filterArr
+}
+
+function matchName(driverArr, argString) {
+    const filterArr = driverArr.filter(driver => driver.name === argString)
+    return filterArr
+}
